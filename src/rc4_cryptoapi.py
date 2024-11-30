06a33745d4c03832873d082f1d3bf3e9c9be7bfa
Huynh Thanh Tam <huynhtam3000@gmail.com>
2024-11-30 12:21:17 +0700

    update file rc4crytoapi

--- src/rc4_cryptoapi.py
+++ src/rc4_cryptoapi.py
@@ -4,26 +4,13 @@ import logging
 from hashlib import sha1
 from struct import pack
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher
-
-try:
-    # NOTE: Avoid DeprecationWarning since cryptography>=43.0
-    # TODO: .algorithm differs from the official documentation
-    from cryptography.hazmat.decrepit.ciphers.algorithms import ARC4
-except ImportError:
-    from cryptography.hazmat.primitives.ciphers.algorithms import ARC4
-
-
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-
 def _makekey(password, salt, keyLength, block, algIdHash=0x00008004):
     r"""
-    Return a intermediate key.
+    Trả về khóa trung gian.
     """
-    # https://msdn.microsoft.com/en-us/library/dd920677(v=office.12).aspx
     password = password.encode("UTF-16LE")
     h0 = sha1(salt + password).digest()
     blockbytes = pack("<I", block)
@@ -34,57 +21,64 @@ def _makekey(password, salt, keyLength, block, algIdHash=0x00008004):
         key = hfinal[: keyLength // 8]
     return key
 
+class RC4:
+    def __init__(self, key):
+        self.key = key
+        self.state = list(range(256))  # Initialize the state array (S-box)
+        self.i = 0
+        self.j = 0
+        # KSA (Key Scheduling Algorithm)
+        key_length = len(key)
+        j = 0
+        for i in range(256):
+            j = (j + self.state[i] + key[i % key_length]) % 256
+            self.state[i], self.state[j] = self.state[j], self.state[i]
+    
+    def _next_byte(self):
+        # Pseudo-random byte generation using the PRGA (Pseudo-Random Generation Algorithm)
+        self.i = (self.i + 1) % 256
+        self.j = (self.j + self.state[self.i]) % 256
+        self.state[self.i], self.state[self.j] = self.state[self.j], self.state[self.i]
+        return self.state[(self.state[self.i] + self.state[self.j]) % 256]
+    
+    def decrypt(self, data):
+        return bytes([b ^ self._next_byte() for b in data])
 
 class DocumentRC4CryptoAPI:
     def __init__(self):
         pass
 
     @staticmethod
-    def verifypw(
-        password,
-        salt,
-        keySize,
-        encryptedVerifier,
-        encryptedVerifierHash,
-        algId=0x00006801,
-        block=0,
-    ):
+    def verifypw(password, salt, keySize, encryptedVerifier, encryptedVerifierHash, algId=0x00006801, block=0):
         r"""
-        Return True if the given password is valid.
+        Trả về True nếu mật khẩu đã cho là hợp lệ.
         """
-        # TODO: For consistency with others, rename method to verify_password or the like
-        # https://msdn.microsoft.com/en-us/library/dd953617(v=office.12).aspx
         key = _makekey(password, salt, keySize, block)
-        cipher = Cipher(ARC4(key), mode=None, backend=default_backend())
-        decryptor = cipher.decryptor()
-        verifier = decryptor.update(encryptedVerifier)
-        verfiferHash = decryptor.update(encryptedVerifierHash)
+        rc4 = RC4(key)
+        verifier = rc4.decrypt(encryptedVerifier)
+        verifierHash = rc4.decrypt(encryptedVerifierHash)
         hash = sha1(verifier).digest()
-        logging.debug([verfiferHash, hash])
-        return hash == verfiferHash
+        logging.debug([verifierHash, hash])
+        return hash == verifierHash
 
     @staticmethod
     def decrypt(password, salt, keySize, ibuf, blocksize=0x200, block=0):
         r"""
-        Return decrypted data.
+        Trả về dữ liệu đã giải mã.
         """
         obuf = io.BytesIO()
 
         key = _makekey(password, salt, keySize, block)
+        rc4 = RC4(key)
 
-        for c, buf in enumerate(iter(functools.partial(ibuf.read, blocksize), b"")):
-            cipher = Cipher(ARC4(key), mode=None, backend=default_backend())
-            decryptor = cipher.decryptor()
-
-            dec = decryptor.update(buf) + decryptor.finalize()
+        for buf in iter(functools.partial(ibuf.read, blocksize), b""):
+            dec = rc4.decrypt(buf)
             obuf.write(dec)
 
-            # From wvDecrypt:
-            # at this stage we need to rekey the rc4 algorithm
-            # Dieter Spaar <spaar@mirider.augusta.de> figured out
-            # this rekeying, big kudos to him
+            # Rekey (RC4 rekeying step)
             block += 1
             key = _makekey(password, salt, keySize, block)
+            rc4 = RC4(key)  # Update RC4 state with the new key
 
         obuf.seek(0)
         return obuf
